327cfb5863d55483fb6250396b791c3922014ab0
Mario Gómez <ma.gomez@duocuc.cl>
2022-05-24 23:14:19 +0000

    .filter() cambiado a .simple_tag()

--- products/templatetags/tag.py
+++ products/templatetags/tag.py
@@ -3,7 +3,7 @@ import requests
 
 register = template.Library()
 
-@register.filter()
+@register.simple_tag()
 def convertir(precio):
    url = 'https://v6.exchangerate-api.com/v6/e066b266eec3a2e468d81caf/pair/CLP/USD/' + str(precio)
 
