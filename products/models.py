d0a806a3bd9292765d03f9452948042a01eb583e
Javiera Azocar <j.azocar@duocuc.cl>
2022-05-25 18:54:15 -0400

    Ingreso de código

--- products/models.py
+++ products/models.py
@@ -4,9 +4,10 @@ from django.utils.text import slugify
 from django.db.models.signals import pre_save
 # Create your models here.
 class Product(models.Model):
+    id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=50)
     description =models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.0) #12121.58
+    price = models.IntegerField(default=0) #12121.58
     slug = models.SlugField(null=False, blank=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='products/', null=False, blank=False)
