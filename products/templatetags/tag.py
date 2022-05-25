7527a9623f6ebb4e90fa42279afba0b75b8c7758
Mario Gómez <ma.gomez@duocuc.cl>
2022-05-25 23:46:59 +0000

    API Actualizada

--- products/templatetags/tag.py
+++ products/templatetags/tag.py
@@ -5,9 +5,9 @@ register = template.Library()
 
 @register.simple_tag()
 def convertir(precio):
-   url = 'https://v6.exchangerate-api.com/v6/e066b266eec3a2e468d81caf/pair/CLP/USD/' + str(precio)
+   url = 'https://v6.exchangerate-api.com/v6/e066b266eec3a2e468d81caf/pair/USD/CLP/' + str(precio)
 
    res = requests.get(url)
    data = res.json()
 
-   return(str(data['conversion_result']))
+   return str(round(data['conversion_result']))
