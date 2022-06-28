f27c91641d8c40e69eceea0d1f81637e29775d4c
Mario Gómez <ma.gomez@duocuc.cl>
2022-06-28 17:35:16 +0000

    Comentarios añadidos

--- tienda/tests.py
+++ tienda/tests.py
@@ -6,11 +6,12 @@ import requests
 # Create your tests here.
 
 class ApiTestCase(TestCase):
-    
+    # Para poner en marcha el test, se crea la función setUp para crear el objeto de prueba con el que se va a trabajar
     def setUp(self):
         print("Función setUp.")
         Product.objects.create(title='Producto de Prueba', price=10000)
 
+    # Se prueba que el precio credo convertido sea igual al precio que se obtiene de la API
     def test_conversion_precios(self):
         print("Funcion de prueba de conversion de precios.")
         url1 = 'https://v6.exchangerate-api.com/v6/e066b266eec3a2e468d81caf/pair/CLP/USD/' + str(Product.objects.get(title='Producto de Prueba').price)
