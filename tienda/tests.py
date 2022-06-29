b486a5a780247a79df8c9137b69dce0e1ff18850
Mario Gómez <ma.gomez@duocuc.cl>
2022-06-29 00:35:05 +0000

    Añadida prueba de conexión a la API

--- tienda/tests.py
+++ tienda/tests.py
@@ -8,16 +8,27 @@ import requests
 class ApiTestCase(TestCase):
     # Para poner en marcha el test, se crea la función setUp para crear el objeto de prueba con el que se va a trabajar
     def setUp(self):
-        print("Función setUp.")
         Product.objects.create(title='Producto de Prueba', price=10000)
+        print("Método setUp: Producto de prueba creado.")
+
+    # Se prueba la conexión a la API
+    def test_api_conexion(self):
+        try:
+            convertir(Product.objects.get(title='Producto de Prueba').price)
+            print("Método test_api_conexion: Conexión exitosa.")
+        except:
+            print("Método test_api_conexion: Conexión fallida.")
+            self.fail("Método test_api_conexion: Conexión fallida.")
 
     # Se prueba que el precio credo convertido sea igual al precio que se obtiene de la API
     def test_conversion_precios(self):
-        print("Funcion de prueba de conversion de precios.")
-        url1 = 'https://v6.exchangerate-api.com/v6/e066b266eec3a2e468d81caf/pair/CLP/USD/' + str(Product.objects.get(title='Producto de Prueba').price)
+        url = 'https://v6.exchangerate-api.com/v6/e066b266eec3a2e468d81caf/pair/CLP/USD/' + str(Product.objects.get(title='Producto de Prueba').price)
 
-        res1 = requests.get(url1)
-        c1 = convertir(Product.objects.get(title='Producto de Prueba').price)
+        res = requests.get(url)
+        conv = convertir(Product.objects.get(title='Producto de Prueba').price)
 
-        data = res1.json()
-        self.assertEqual(c1, str(round(data['conversion_result'],1)))
+        data = res.json()
+        print("Resultado de la API: $" + str(round(data['conversion_result'],2)) + " \nResultado de la función: $" + str(conv))
+        self.assertEqual(conv, str(round(data['conversion_result'],2)))
+
+        
